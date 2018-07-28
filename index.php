44af4189778f6fdcf497d24a57749c507fb0af06
Luis Miguel <luis.miguelbuendia.07@gmail.com>
2018-07-27 22:50:18 -0500

    1.1

--- index.php
+++ index.php
@@ -47,7 +47,8 @@ con respecto a su padre (container).
         <div class="col-item-flex-3 ">
             <figure class="header-logo">
                 <a href="#PORTADA">
-                    <span class="header-logo-a f-segoe-r">TIC</span><span class="header-logo-b f-segoe-b">STARK.</span>
+                    <span class="header-logo-a f-segoe-r">TIC</span>
+                    <span class="header-logo-b f-segoe-b">STARK.</span>
                 </a>
             </figure>
         </div>
@@ -89,13 +90,13 @@ con respecto a su padre (container).
                     <h1 class="f-segoe-sb a">CREATIVIDAD</h1>
                 </div>
                 <div class="col-item-auto">
-                    <h2 class="f-segoe-sl b">NUESTRA PRIORIDAD</h1>
+                    <h2 class="f-segoe-sl-webkit f-segoe-sl  b">NUESTRA PRIORIDAD</h1>
                 </div>
                 <div class="col-item-auto">
                     <h2 class="f-segoe-r c">MARCA / DISEÑO / DESARROLLO</h2>
                 </div>
                 <div class="col-item-auto boton">
-                    <a class="button f-segoe-sl" href="sketch.html">LEER MÁS</a>
+                    <a class="button f-segoe-sl f-segoe-sl-webkit" href="sketch.html">LEER MÁS</a>
                 </div>
             </figure>
         </div>
@@ -104,146 +105,185 @@ con respecto a su padre (container).
     <main class="main_content">
 
         <!-- SERVICIOS DE TICSTARK -->
-        <section class="container-flex servicios">
+        <section class="container-fluido">
+            <div class="container-flex servicios">
+                <div class="efecto-caja bloque-servicios col-item-flex-3 background-color-ts-5">
+                    <figure class="container-flex flex-grow-1 flexflow-c-ol nowrap-align-center">
+                        <div class="col-item-auto all-color-ticstark-white">
+                            <span class="f-segoe-r b-1" href="#">POSICIONAMOS</span>
+                        </div>
+                        <div class="col-item-auto all-color-ticstark-white">
+                            <span class="f-segoe-r a-1" href="#">TU MARCA</span>
+                        </div>
+                    </figure>
+                </div>
 
-            <div class="efecto-caja-1 bloque-servicios col-item-flex-3 background-color-ts-5">
-                <figure class="container-flex flex-grow-1 flexflow-c-ol nowrap-align-center">
-                    <div class="col-item-auto all-color-ticstark-white">
-                        <span class="f-segoe-r b-1" href="#">POSICIONAMOS</span>
-                    </div>
-                    <div class="col-item-auto all-color-ticstark-white">
-                        <span class="f-segoe-r a-1" href="#">TU MARCA</span>
-                    </div>
-                </figure>
-            </div>
+                <div class="efecto-caja bloque-servicios col-item-flex-3 background-color-ts-5-1">
+                    <figure class="container-flex flex-grow-1 flexflow-c-ol nowrap-align-center">
+                        <div class="col-item-auto all-color-ticstark-white">
+                            <span class="f-segoe-r a-1" href="#">CREAMOS</span>
+                        </div>
+                        <div class="col-item-auto all-color-ticstark-white">
+                            <span class="f-segoe-r b-1" href="#">COMUNIDADES</span>
+                        </div>
+                    </figure>
+                </div>
 
-            <div class="efecto-caja-2 bloque-servicios col-item-flex-3 background-color-ts-5-1">
-                <figure class="container-flex flex-grow-1 flexflow-c-ol nowrap-align-center">
-                    <div class="col-item-auto all-color-ticstark-white">
-                        <span class="f-segoe-r a-1" href="#">CREAMOS</span>
-                    </div>
-                    <div class="col-item-auto all-color-ticstark-white">
-                        <span class="f-segoe-r b-1" href="#">COMUNIDADES</span>
-                    </div>
-                </figure>
-            </div>
+                <div class="efecto-caja bloque-servicios col-item-flex-3 background-color-ts-5-2">
+                    <figure class="container-flex flex-grow-1 flexflow-c-ol nowrap-align-center">
+                        <div class="col-item-auto all-color-ticstark-white">
+                            <span class="f-segoe-r b-1" href="#">GENERAMOS</span>
+                        </div>
+                        <div class="col-item-auto all-color-ticstark-white">
+                            <span class="f-segoe-r a-1" href="#">LEADS</span>
+                        </div>
+                    </figure>
+                </div>
 
-            <div class="efecto-caja-3 bloque-servicios col-item-flex-3 background-color-ts-5-2">
-                <figure class="container-flex flex-grow-1 flexflow-c-ol nowrap-align-center">
-                    <div class="col-item-auto all-color-ticstark-white">
-                        <span class="f-segoe-r b-1" href="#">GENERAMOS</span>
-                    </div>
-                    <div class="col-item-auto all-color-ticstark-white">
-                        <span class="f-segoe-r a-1" href="#">LEADS</span>
-                    </div>
-                </figure>
-            </div>
-
-            <div class="efecto-caja-4 bloque-servicios col-item-flex-3 background-color-ts-5-3">
-                <figure class="container-flex flex-grow-1 flexflow-c-ol nowrap-align-center">
-                    <div class="col-item-auto all-color-ticstark-white">
-                        <span class="f-segoe-r a-1" href="#">DISEÑAMOS</span>
-                    </div>
-                    <div class="col-item-auto all-color-ticstark-white">
-                        <span class="f-segoe-r b-1" href="#">ESTRATEGIAS DIGITALES</span>
-                    </div>
-                </figure>
+                <div class="efecto-caja bloque-servicios col-item-flex-3 background-color-ts-5-3">
+                    <figure class="container-flex flex-grow-1 flexflow-c-ol nowrap-align-center">
+                        <div class="col-item-auto all-color-ticstark-white">
+                            <span class="f-segoe-r a-1" href="#">DISEÑAMOS</span>
+                        </div>
+                        <div class="col-item-auto all-color-ticstark-white">
+                            <span class="f-segoe-r b-1" href="#">ESTRATEGIAS DIGITALES</span>
+                        </div>
+                    </figure>
+                </div>
             </div>
         </section>
 
         <!-- SERVICIOS DETALLADOS -->
-        <section class="container-flex background-color-ts-6">
-            <article class="col-item-flex-4">
-                <img class="img-width-full" src="assets/img/compreso/path-5.png">
-            </article>
-            <article class="col-item-flex-8">
-                <div class="container-flex flexflow-c-ol sr">
-                    <div class="col-item-auto">
-                        <h1 class="f-segoe-sl a-2">IDENTIDAD DE MARCA</h1>
+        <section class="container-fluido background-color-ts-6">
+            <div class="container-flex">
+                <article class="col-item-flex-4">
+                    <img class="img-full" src="assets/img/compreso/path-5.png">
+                </article>
+                <article class="col-item-flex-8">
+                    <div class="container-flex flexflow-c-ol sr">
+                        <div class="col-item-auto">
+                            <h1 class="f-segoe-sl f-segoe-sl-webkit a-2">IDENTIDAD DE MARCA</h1>
+                        </div>
+                        <div class="col-item-auto">
+                            <p class="f-segoe-r b-2">Le damos identidad a tu marca, teniendo claro el segmento de mercado y tu público objetivo.</p>
+                        </div>
                     </div>
-                    <div class="col-item-auto">
-                        <p class="f-segoe-r b-2">Le damos identidad a tu marca, teniendo claro el segmento de mercado y tu público objetivo.</p>
-                    </div>
-                </div>
-            </article>
+                </article>
+            </div>
         </section>
 
-        <section class="container-flex flexflow-r-ol background-white">
-            <article class="col-item-flex-8">
-                <div class="container-flex flexflow-c-ol sr">
-                    <div class="col-item-auto">
-                        <h1 class="f-segoe-sl a-2">MARKETING DIGITAL</h1>
+        <section class="container-fluido background-white">
+            <div class="container-flex">
+                <article class="col-item-flex-8">
+                    <div class="container-flex flexflow-c-ol sr">
+                        <div class="col-item-auto">
+                            <h1 class="f-segoe-sl f-segoe-sl-webkit a-2">MARKETING DIGITAL</h1>
+                        </div>
+                        <div class="col-item-auto">
+                            <p class="f-segoe-r b-2">Diseñamos estrategias y usamos herramientas para llegar a tu público verdadero, siempre ofreciendo
+                                lo que ellos necesitan de tu marca.</p>
+                        </div>
                     </div>
-                    <div class="col-item-auto">
-                        <p class="f-segoe-r b-2">Diseñamos estrategias y usamos herramientas para llegar a tu público verdadero, siempre ofreciendo
-                            lo que ellos necesitan de tu marca.</p>
-                    </div>
-                </div>
-            </article>
-            <article class="col-item-flex-4">
-                <img class="img-width-full" src="assets/img/compreso/path-6.png">
-            </article>
+                </article>
+                <article class="col-item-flex-4">
+                    <img class="img-full" src="assets/img/compreso/path-6.png">
+                </article>
+            </div>
         </section>
 
-        <section class="container-flex background-color-ts-7">
-            <article class="col-item-flex-4">
-                <img class="img-width-full" src="assets/img/compreso/path-7.png">
-            </article>
-            <article class="col-item-flex-8">
-                <div class="container-flex flexflow-c-ol sr">
-                    <div class="col-item-auto">
-                        <h1 class="f-segoe-sl a-2">REDES SOCIALES</h1>
+        <section class="container-fluido background-color-ts-7">
+            <div class="container-flex">
+                <article class="col-item-flex-4">
+                    <img class="img-full" src="assets/img/compreso/path-7.png">
+                </article>
+                <article class="col-item-flex-8">
+                    <div class="container-flex flexflow-c-ol sr">
+                        <div class="col-item-auto">
+                            <h1 class="f-segoe-sl f-segoe-sl-webkit a-2">REDES SOCIALES</h1>
+                        </div>
+                        <div class="col-item-auto">
+                            <p class="f-segoe-r b-2">Creamos contenido para las comunidades digitales, en Facebook e Instagram, que se identifiquen
+                                con tu marca.</p>
+                        </div>
                     </div>
-                    <div class="col-item-auto">
-                        <p class="f-segoe-r b-2">Creamos contenido para las comunidades digitales, en Facebook e Instagram, que se identifiquen con
-                            tu marca.</p>
-                    </div>
-                </div>
-            </article>
+                </article>
+            </div>
         </section>
 
         <!-- BLOG (PERMITE CAMBIOS EN ELEMENTOS CON FLEX) -->
-        <section class="background-color-ts-1" id="BLOG">
+        <section class="container-fluido background-color-ts-8 blog" id="BLOG">
             <article class="text-container">
-                <h2 class="subtitulo-c f-nexa-b">BLOG</h2>
+                <h2 class="c-2 f-segoe-bn color-ticstark-black">BLOG</h2>
             </article>
-        
-            <div class="container-flex flexflow-r-ol fitems-center">
-                <article class="col-item-auto">
-                    <img class="img-width-margin" src="assets/img/compreso/path-2.png">
+            <div class="container-flex flexflow-r-ol fitems-spacearound">
+                <article class="col-item-flex-3">
+                    <div class="container-grid">
+                        <span class="cateogoria-container">Marca Personal</span>
+                        <img class="img-full" src="assets/img/compreso/path-2.png">
+                    </div>
                 </article>
-                <article class="col-item-auto">
-                    <img class="img-width-margin" src="assets/img/compreso/path-3.png">
+                <article class="col-item-flex-3">
+                    <div class="container-grid">
+                        <span class="cateogoria-container">Desafia tu mundo</span>
+                        <img class="img-full" src="assets/img/compreso/path-3.png">
+                    </div>
                 </article>
-                <article class="col-item-auto">
-                    <img class="img-width-margin" src="assets/img/compreso/path-4.png">
+                <article class="col-item-flex-3">
+                    <div class="container-grid">
+                        <span class="cateogoria-container">Desarrollo de tecnologías</span>
+                        <img class="img-full" src="assets/img/compreso/path-4.png">
+                    </div>
                 </article>
             </div>
         </section>
 
         <!-- PARTNERS/CLIENTES (PERMITE CAMBIOS EN ELEMENTOS CON FLEX) -->
-        <section class="container-flex" id="CUSTOMERS">
-            <div class=" container-flex flexflow-f-ol all-color-ticstark-1 background-color-ts-3">
-                <article class="col-3 d-flex-item text-container">
-                    <h2 class="subtitulo-c f-nexa-b">NUESTROS CLIENTES</h2>
+        <section class="container-fluido background-color-ts-9 customer" id="CUSTOMER">
+            <div class=" container-flex nowrap-align-center fitems-spacearound">
+                <article class="col-item-flex-3">
+                    <h2 class="c-2 f-segoe-bn color-ticstark-white">CLIENTES</h2>
                 </article>
+                <div class="col-item-9">
+                    <div class="container-flex flexflow-r-pl fitems-spacearound">
+                        <article class="col-item-flex-2">
+                            <img class="img-full-custom" src="assets/img/svg-images/ibm.svg">
+                        </article>
+                        <article class="col-item-flex-2">
+                            <img class="img-full-custom" src="assets/img/svg-images/logo-platzi.png">
+                        </article>
+                        <article class="col-item-flex-2">
+                            <img class="img-full-custom" src="assets/img/svg-images/nasa-6.svg">
+                        </article>
+                    </div>
+                </div>
 
-                <article class="item-container col-9 d-flex-item">
-                    <img class="img-width-full" src="assets/img/customers/clientes-2.png">
-                </article>
             </div>
         </section>
 
         <!-- FORMULARIO DE CONTACTO (PERMITE CAMBIOS EN ELEMENTOS CON FLEX)-->
-        <section class="container-flex all-color-ticstark-1 background-color-ts-4" id="FAQ">
-            <article class="text-container flex-item-45">
-                <h2 class="subtitulo-c f-nexa-b">CONTACTENOS (FOOTER)</h2>
-            </article>
-            <article class="item-container flex-item-55">
-                <a class="link" href="https://www.facebook.com/groups/monteriavende">Facebook</a>
-            </article>
+        <section class="container-fluido" id="FAQ">
+            <div class="container-flex">
+                <div class="col-item-flex-4 background-color-ts-8">
+                    <div class="sr3 container-flex flexflow-c-ol nowrap-align-center">
+                        <h2 class="c-2 f-segoe-bn color-ticstark-6">CONTACTO</h2>
+                        <ul class="lista-none">
+                            <li>info@ticstark.com</li>
+                            <li>3003422392</li>
+                            <li>www.ticstark.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-item-flex-8 background-color-ts-7">
+                    <div class="sr3 container-flex color-ticstark-6">
+                        <h2 class="c-3 f-segoe-r ">ENVÍANOS TU MENSAJE</h2>
+                    </div>
+                </div>
+            </div>
         </section>
     </main>
+
+    <!-- FOOTER -->
+    <footer class="container-fluido"></footer>
 </body>
 
 </html>
